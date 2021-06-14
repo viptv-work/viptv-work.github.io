08e8a60e82847304162b4c6617a07a44c4f50a60
qq2236639958 <2236639958@qq.com>
2021-06-15 04:33:33 +0800

    add

--- README.md
+++ README.md
@@ -1,7 +1,58 @@
-# VIPTV 3.0 网络电视
+viptv 2021.05 
+===============
 
-![VIPTV CI](https://github.com/vuejs/docs-next-zh-cn/workflows/Vue-docs%20CI/badge.svg)     
+> 本站为VIPTV开发版的WEB体验,对标聚合海量电视， 直播，影视，短片，音乐，小说，壁纸等资源。底层架构做了进一步的优化升级，不忘初心、匠心打造、值得信赖！
 
-该站点基于 VUE3.0 构建而成。网站内容在 src 文件夹内，格式为 Markdown。欢迎 issue 或 pull request。
+## 技术栈
 
+- [Vue Cli（Vue 脚手架工具）](https://cli.vuejs.org/zh/)
+- [Vue（核心框架）](https://cn.vuejs.org/)
+- [Vue Router（页面路由）](https://router.vuejs.org/zh/)
+- [Vuex（状态管理）](https://vuex.vuejs.org/zh/)
+- ES 6 / 7 （JavaScript 语言的下一代标准）
+- Less（CSS 预处理器）
+- Axios（网络请求）
+- FastClick（解决移动端 300ms 点击延迟）
+
+## 主要内容
+
+ * 咪咕直播 
+ * 北邮直播
+ * 央视频源
+ * 百度好看
+ * 咪咕视频
+ * 凤凰视频
+ * 酷狗音乐
+ * 酷我铃音
+ * 自定义源
+
+## VipTV的运行环境 : NodeJs
+
+## NPM 安装包库
+
+~~~
+npm i
+~~~
+
+## NPM 启动服务
+
+~~~
+npm run serve
+~~~
+
+## 浏览器中访问
+
+~~~
+http://127.0.0.1
+~~~
+
+## NPM 打包上传
+
+~~~
+npm run build
+~~~
+
+## 开发版 - 版本说明
++ 1 如果你默认开启https，但是本站聚合的资源也有http的，所以在体验过程当中有影视资源播放不了，纯属正常.
++ 2 由于本站正在发中，代码暂不开源，若有需要，请联系作者。
 
---
+++ docs/json/base/zycj.js
@@ -0,0 +1,88 @@
+{
+   "m3u8": [{
+       "id": 1,
+       "api": "https://4k.4kyima.com/api.php/provide/vod/at/xml"
+     },
+     {
+       "id": 2,
+       "api": "http://www.kuaibozy.com/api.php/provide/vod/from/kbm3u8/at/xml/",
+       "jiexiUrl": "https://jx.7kjx.com/?url="
+     },
+     {
+       "id": 3,
+       "api": "https://m3u8.apibdzy.com/api.php/provide/vod/at/xml/",
+       "jiexiUrl": "https://jx.444662.cn/m3u8/?url="
+     },
+     {
+       "id": 4,
+       "api": "https://api.tiankongapi.com/api.php/provide/vod/at/xml/from/tkm3u8/",
+       "jiexiUrl": "https://jx.7kjx.com/?url="
+     },
+     {
+       "id": 5,
+       "api": "http://cj.bajiecaiji.com/inc/bjm3u8.php"
+     },
+     {
+       "id": 6,
+       "api": "https://www.rrzyw.cc/api.php/provide/vod/from/rrm3u8/at/xml/",
+       "jiexiUrl": "https://jx.7kjx.com/?url="
+     }
+   ],
+   "html": [{
+       "api": "https://www.39kan.com/api.php/provide/vod/at/xml"
+     },
+     {
+       "api": "https://v.dtjos.com/api.php/provide/vod/at/xml"
+     },
+     {
+       "api": "http://maguays.cc/api.php/provide/vod/at/xml/"
+     },
+     {
+       "api": "https://zy.kuluni.cn/api.php/provide/vod/at/xml"
+     },
+     {
+       "api": "http://www.moguzyw.com:520/moguzy.php/provide/vod/at/xml"
+     },
+     {
+       "api": "http://zy.zcocc.com/api.php/provide/vod/at/xml"
+     }
+   ],
+   "modifya": [{
+     "api": "http://shidian.vip/api.php/provide/vod/at/xml"
+   }],
+   "xxx": [{
+       "api": "http://cj.cangtiancj.com/api.php/Seacms/vod/"
+     },
+     {
+       "api": "https://lbapi9.com/api.php/provide/vod/at/xml/"
+     },
+     {
+       "api": "http://m.7777688.com/inc/api.php"
+     },
+     {
+       "api": "https://shayuapi.com/api.php/Seacms/vod/"
+     },
+     {
+       "api": "https://dadiapi.com/api.php"
+     },
+     {
+       "api": "http://api.11bat.com/mac10.php"
+     },
+     {
+       "api": "http://cj.809zy.com/inc/zyapimac.php"
+     },
+     {
+       "api": "http://wmcj8.com/inc/sapi.php"
+     },
+     {
+       "api": "https://jialiapi.com/api.php/provide/vod/at/xml/",
+       "json": "https://jialiapi.com/api.php/provide/vod/"
+     },
+     {
+       "api": "https://www.ugvapi.com/inc/zyapimac.php"
+     },
+     {
+       "api": "http://m.7777688.com/inc/api.php"
+     }
+   ]
+ }
